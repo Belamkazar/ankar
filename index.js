dc80c7bb663962c717426fe1b5056cb1ca0239b4
Belamkazar <126763856+Belamkazar@users.noreply.github.com>
2023-08-01 07:25:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ client.on('message', async message => {
 async function initializeClient() {
   const browser = await puppeteer.launch(puppeteerOptions);
   await client.initialize(browser);
+
+  // Acceder a la propiedad 'page' del cliente una vez que está inicializado
   await client.page.goto('https://web.whatsapp.com/', {
     waitUntil: 'domcontentloaded', // Esperar a que el DOM esté completamente cargado
   });
